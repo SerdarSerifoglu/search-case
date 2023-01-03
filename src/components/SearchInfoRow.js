0054cb2c8d921f42ecc7c172c7a52ec5f2e2969c
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2023-01-03 19:46:48 +0300

    Create SearchInfoRow.js

---
+++ src/components/SearchInfoRow.js
@@ -0,0 +1,33 @@
+import React from "react";
+import SearchIconText from "./SearchIconText";
+
+function SearchInfoRow({ personalData }) {
+  return (
+    <>
+      <section key={personalData.id}>
+        <div className="container">
+          <h5>{personalData.name}</h5>
+          <div className="search-info-wrapper col-sm-12">
+            <SearchIconText
+              classNameData="col-sm-4"
+              icon="phone"
+              text={personalData.phone}
+            ></SearchIconText>
+            <SearchIconText
+              classNameData="col-sm-4"
+              icon="at"
+              text={personalData.email}
+            ></SearchIconText>
+            <SearchIconText
+              classNameData="col-sm-4"
+              icon="file"
+              text={personalData.policyNumber + "|" + personalData.policyNumber}
+            ></SearchIconText>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default SearchInfoRow;
