215f5f7be961fb79f56de76fc1af953fa7cdfdc5
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2023-01-03 19:46:27 +0300

    Create staticData.js

---
+++ src/staticData.js
@@ -0,0 +1,37 @@
+export const staticData = [
+  {
+    id: 1,
+    name: "Ayşe Kara",
+    policyNumber: "123456789",
+    phone: "+90 511 111 11 11",
+    email: "aysekara@ggggmail.com",
+  },
+  {
+    id: 2,
+    name: "Ahmet Uğur",
+    policyNumber: "99568756",
+    phone: "+90 511 111 11 12",
+    email: "ahmetugur@ggggmail.com",
+  },
+  {
+    id: 3,
+    name: "Serdar Şerifoğlu",
+    policyNumber: "534545555535",
+    phone: "+90 537 111 11 13",
+    email: "serdarserifoglu@ggggmail.com",
+  },
+  {
+    id: 4,
+    name: "Eren Görgü",
+    policyNumber: "09576556767",
+    phone: "+90 537 111 11 14",
+    email: "erengorgu@hhhhotmail.com",
+  },
+  {
+    id: 5,
+    name: "Deniz Ünal",
+    policyNumber: "0013983891",
+    phone: "+90 537 111 11 15",
+    email: "denizunal@yaaaandex.com",
+  },
+];
