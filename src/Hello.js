e35898cdf91b8e88001ab5f883526a3250b34795
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2023-01-03 15:50:29 +0300

    bootstrap add

--- src/Hello.js
+++ src/Hello.js
@@ -3,6 +3,10 @@ import React from "react";
 export default ({ name }) => (
   <>
     <h1>Hello {name}!</h1>
+    <div className="row">
+      <div className="col-md-6">Ömer</div>
+      <div className="col-md-6">Serdar</div>
+    </div>
     <p>Start editing and see your changes reflected here immediately!</p>
   </>
 );
